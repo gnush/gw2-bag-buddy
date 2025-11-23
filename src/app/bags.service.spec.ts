a0fee6eacc0a4bc60861a54d2b2a712332d4333d
gnush <1129861+gnush@users.noreply.github.com>
2025-11-23 12:01:26 +0100

    spec

--- src/app/bags.service.spec.ts
+++ src/app/bags.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Bags } from './bags.service';
+import { BagsService } from './bags.service';
 
 describe('Bags', () => {
-  let service: Bags;
+  let service: BagsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Bags);
+    service = TestBed.inject(BagsService);
   });
 
   it('should be created', () => {
